0b571dc30784ae9240d9ecf9ed45c5478d680bae
lpm <lawrie.pm91@gmail.com>
2020-09-08 17:58:55 +0100

    Create route, URL and component for detail page

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,5 @@
 const pics = () => '/pics';
+pics.detail = (id = ':picsId') => `/pics/${id}`;
 
 export default {
   pics,
--- src/views/app/index.js
+++ src/views/app/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import List from 'views/list';
+import Detail from 'views/detail';
 import urls from 'urls';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path={urls.pics()}><List /></Route>
+        <Route exact path={['/', urls.pics()]}><List /></Route>
+        <Route exact path={urls.pics.detail()}><Detail /></Route>
       </Switch>
     </Router>
   )
